5d061d2273397304e921763c5ddb1d3c9a2e8c41
Eric McAllister <ericmcallister@Mr-White.local>
2014-02-09 14:18:13 -0700

    adding sample plugin

---
+++ wordpress/wp-content/plugins/sample-plugin/sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+ /*
+ * Plugin Name: Test plugin
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A brief description of the Plugin.
+ * Version: 1.0
+ * Author: Eric McAllister
+ *
+echo "HELLO WORLD!";
+?>/
